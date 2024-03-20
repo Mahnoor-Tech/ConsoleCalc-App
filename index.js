a7fc81d347fa9c2afc4e8e14513500f4188e0937
Mahnoor-Tech <mahnoor.tech486@gmail.com>
2024-03-20 22:23:45 +0500

    consoleCalc

---
+++ index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+console.log(`\n-- Welcome to the Command Line Calculator App --`);
+console.log(`\n\t"Here math meets simplicity!"`);
+console.log(`_________________________________________________\n`);
+const answer = await inquirer.prompt([
+    { message: "Enter the first number:", type: "number", name: "firstNumber" },
+    { message: "Enter the second number:", type: "number", name: "secondNumber" },
+    {
+        message: "Choose an operator (+, -, *, /):",
+        type: "list",
+        name: "operator",
+        choices: [
+            "Addition (+)",
+            "Subtraction (-)",
+            "Multiplication (*)",
+            "Division (/)",
+        ],
+    },
+]);
+// Destructuring
+const { firstNumber, secondNumber, operator } = answer;
+// Result
+let output;
+if (operator === "Addition (+)") {
+    output = firstNumber + secondNumber;
+}
+else if (operator === "Subtraction (-)") {
+    output = firstNumber - secondNumber;
+}
+else if (operator === "Multiplication (*)") {
+    output = firstNumber * secondNumber;
+}
+else if (operator === "Division (/)") {
+    if (secondNumber === 0) {
+        console.log("*Error: Cannot divide by zero!");
+    }
+    else {
+        output = firstNumber / secondNumber;
+    }
+}
+else {
+    console.log("*Error: Invalid Operator!");
+}
+console.log("---------------------------------");
+console.log(`O U T P U T: ${output}`);
+console.log("---------------------------------");
